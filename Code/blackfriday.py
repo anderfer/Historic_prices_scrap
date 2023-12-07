e3c400604e58b2bd1f3bcfcb1d6e0fe317edd24f
Ander <fernandezander1@gmail.com>
2023-12-07 13:24:43 +0100

    Actualizado

---
+++ Code/blackfriday.py
@@ -0,0 +1,6 @@
+
+            
+import streamlit as st
+           
+st.markdown("<iframe title="Dashboard BlackFriday" width='100%' height='100%' src="https://app.powerbi.com/reportEmbed?reportId=dc5a3ebe-f5d6-4917-b189-7b26bef61773&autoAuth=true&ctid=62b8e19a-039a-445f-908d-acf0744462f5" frameborder="0" allowFullScreen="true"></iframe>", unsafe_allow_html=True)
+  
